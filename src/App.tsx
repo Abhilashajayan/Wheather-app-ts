f54b1221d925022eedb29317ec68d7926567cbfe
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2023-12-06 22:24:06 +0530

    initial push

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Video from './component/video';
+
+
+const App: React.FC = () => {
+  return (
+    <div className='relative w-screen h-screen overflow-hidden'>
+      <Video/>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/video.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const video : React.FC = () => {
+  return (
+    <>
+    <video
+      className='absolute inset-0 w-full h-full object-cover object-center'
+      autoPlay
+      muted
+      loop
+    >
+     <source src='https://res.cloudinary.com/dyawq6e7r/video/upload/v1701448143/mist_m7bxfh.mp4' type='video/mp4' />
+
+    </video>
+    <div className='relative bg-blue-500 w-screen h-screen bg-opacity-50 p-4 z-10'>
+    </div>
+  </>
+  )
+}
+
+export default video
